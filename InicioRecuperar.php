be5b4d7b9b4d0ae29da7292897a2aa3b62240ea1
walter <walter.m.d92@gmail.com>
2020-08-31 12:32:43 -0300

    modificaciones en recupero de password funcionando

--- InicioRecuperar.php
+++ InicioRecuperar.php
@@ -1,37 +1,55 @@
 <?php
-require("class.phpmailer.php");
-require("class.smtp.php");
-require("conexion.php");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'conexion.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 $user=$_POST['usuario'];
 $mensaje="recupere su clave";
-if (isset($_POST['send'])){
- include("sendmail.php");//Mando a llamar la funcion que se encarga de enviar el correo electronico
- $consulta=mysqli_query($conexion,"SELECT * FROM personascontactos WHERE descripcion='$user'");
- if($r=mysqli_fetch_array($consulta)){
-     $idPersona=$r['idPersona'];
-     echo "enviar email a ".$r['descripcion'];
-     $token=uniqid();
-     $sql=mysqli_query($conexion,"UPDATE personas SET token='$token' WHERE idPersona=$idPersona");
-	 $smtpHost = "smtp.gmail.com";  // Dominio alternativo brindado en el email de alta 
-     $smtpUsuario = ("tuspelisfc@gmail.com");  // Mi cuenta de correo
-     $smtpClave = "Pelisfc_1997";  // Mi contraseña
-     $mail = new PHPMailer();
-     $mail->IsSMTP();
-     $mail->SMTPAuth = true;
-     $mail->Port = 587; 
-     $mail->IsHTML(true); 
-     $mail->CharSet = "utf-8";
 
-     // VALORES A MODIFICAR //
-     $mail->Host = $smtpHost; 
-     $mail->Username = $smtpUsuario; 
-     $mail->Password = $smtpClave;
-     $mail->setFrom = $smtpUsuario;
-     $mail->FromName= "tuspelisfc"; // Email desde donde envío el correo.
-     $mail->AddAddress($user); // Esta es la dirección a donde enviamos los datos del formulario
-     $mail->Subject = "formulario de reestablecimiento de contraseña para GestiStock"; // Este es el titulo del email.
-     $mensajeHtml = nl2br($mensaje);
-     $mail->Body = "<html> 
+if (isset($_POST['send'])&& !empty($_POST['send'])) {
+	$consulta=mysqli_query($conexion,"SELECT idPersona FROM personascontactos WHERE descripcion='$user'");
+	while($r=mysqli_fetch_array($consulta)){
+		$idPersona=$r['idPersona'];
+		$token=uniqid();
+		$sql=mysqli_query($conexion,"UPDATE personas SET token='$token' WHERE idPersona=$idPersona");
+		}else{
+		    header("location:index.php?recuperar=3");
+		 }
+			
+		}
+
+		try {
+		    //Server settings
+		    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+		    $mail->isSMTP();                                            // Send using SMTP
+		    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+		    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+		    $mail->Username   = 'tuspelisfc@gmail.com';                     // SMTP username
+		    $mail->Password   = 'Pelisfc_1997';                               // SMTP password
+		    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+		    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+		    //Recipients
+		    $mail->setFrom('tuspelisfc@gmail.com', 'noReply');
+		    $mail->addAddress($user);     // Add a recipient
+		    //$mail->addAddress('ellen@example.com');               // Name is optional
+		    
+
+		    // Attachments
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+		   // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+		    // Content
+		    $mail->isHTML(true);                                  // Set email format to HTML
+		    $mail->Subject = 'formulario de reestablecimiento de contraseña para GestiStock';
+		    $mensajeHtml = nl2br($mensaje);
+		    $mail->Body  = "<html> 
                         <body> 
                              <h1 align='center'>GestiStock</h1>
                              <div style='background:black;color:white;padding:20px'><h2>Solicitud de restablecimiento de contraseña</h2></div>
@@ -41,25 +59,18 @@ if (isset($_POST['send'])){
                              <a href='http://localhost/EQUIPO%20B/recuperar.php?token=$token'>Haz clic aquí para restablecer tu contraseña</a>
                         </body> 
                     </html>
-                    <br />"; // Texto del email en formato HTML
-     $mail->AltBody = "{$mensaje} \n\n "; // Texto sin formato HTML
-     $mail->SMTPOptions = array(
-     'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-       )
-     );
-     $estadoEnvio = $mail->Send(); 
-     if($estadoEnvio){
-          header("location:index.php?recuperar=1");
-     } else {
-          header("location:index.php?recuperar=2");
+                    <br />";
+		    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+		    $mail->send();
+		    header("location:index.php?recuperar=1");
+		} catch (Exception $e) {
+		    //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+		    header("location:index.php?recuperar=2");
            exit();
-     }
- }else{
-    header("location:index.php?recuperar=3");
- }
-}
-        
+		}
+
+	
+}//termina if del 
+
 ?>
