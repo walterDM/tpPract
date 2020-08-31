de1d1db66de38e6df73d665f4857278f11963eeb
fabricabj <colavella23@gmail.com>
2020-08-31 17:19:03 -0300

    modificado por fabricio

--- InicioRecuperar.php
+++ InicioRecuperar.php
@@ -14,11 +14,11 @@ $mensaje="recupere su clave";
 
 if (isset($_POST['send'])&& !empty($_POST['send'])) {
 	$consulta=mysqli_query($conexion,"SELECT idPersona FROM personascontactos WHERE descripcion='$user'");
-	while($r=mysqli_fetch_array($consulta)){
+	if($r=mysqli_fetch_array($consulta)){
 		$idPersona=$r['idPersona'];
 		$token=uniqid();
 		$sql=mysqli_query($conexion,"UPDATE personas SET token='$token' WHERE idPersona=$idPersona");
-		}
+		
 			
 		
 
@@ -29,7 +29,14 @@ if (isset($_POST['send'])&& !empty($_POST['send'])) {
 		    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
 		    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
 		    $mail->Username   = 'tuspelisfc@gmail.com';                     // SMTP username
-		    $mail->Password   = 'Pelisfc_1997';                               // SMTP password
+			$mail->Password   = 'Pelisfc_1997';                             // SMTP password
+			$mail->SMTPOptions = array(
+				'ssl' => array(
+					'verify_peer' => false,
+					'verify_peer_name' => false,
+					'allow_self_signed' => true
+				)
+			);                              
 		    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 		    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
@@ -47,28 +54,34 @@ if (isset($_POST['send'])&& !empty($_POST['send'])) {
 		    $mail->isHTML(true);                                  // Set email format to HTML
 		    $mail->Subject = 'formulario de reestablecimiento de contraseña para GestiStock';
 		    $mensajeHtml = nl2br($mensaje);
-		    $mail->Body  = "<html> 
-                        <body> 
-                             <h1 align='center'>GestiStock</h1>
-                             <div style='background:black;color:white;padding:20px'><h2>Solicitud de restablecimiento de contraseña</h2></div>
-                             <p>Alguien ha solicitado una nueva contraseña para la siguiente cuenta en GestiStock</p>
-                             <p>usuario: {$r['descripcion']}</p>
-                             <p>Si no hiciste esta solicitud simplemente ignora este correo electrónico. Si quiere proceder: </p>
-                             <a href='http://localhost/EQUIPO%20B/recuperar.php?token=$token'>Haz clic aquí para restablecer tu contraseña</a>
-                        </body> 
-                    </html>
-                    <br />";
+			$mail->Body  = "<html> 
+			                 <head>
+			                   <meta charset='utf-8'>
+			                 </head>
+                             <body> 
+                                <h1 align='center'>GestiStock</h1>
+                                <div style='background:black;color:white;padding:20px'><h2>Solicitud de restablecimiento de contraseña</h2></div>
+                                <p>Alguien ha solicitado una nueva contraseña para la siguiente cuenta en GestiStock</p>
+                                <p>usuario: {$r['descripcion']}</p>
+                                <p>Si no hiciste esta solicitud simplemente ignora este correo electrónico. Si quiere proceder: </p>
+                                <a href='http://localhost/EQUIPO%20B/recuperar.php?token=$token'>Haz clic aquí para restablecer tu contraseña</a>
+                             </body> 
+                           </html>
+                           <br />";
 		    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 		    $mail->send();
 		    header("location:index.php?recuperar=1");
 		} catch (Exception $e) {
-		    //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+		    // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 		    header("location:index.php?recuperar=2");
            exit();
 		}
 
 	
+ }else{
+	 header("location:index.php?recuperar=3");
+ }
 }//termina if del 
 
 ?>
