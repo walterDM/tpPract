fba5b0059bf2daca9bfc183ccdaec9dcdb75d7bc
walter <walter.m.d92@gmail.com>
2020-09-04 16:40:29 -0300

    footer y header ordenados

--- footer.php
+++ footer.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
-   <head>
-     <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	  <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
-	  <link rel="stylesheet" type="text/css" href="bootstrap-4.3.1-dist/css/estilos.css">
-    
-   </head>
-   <body>
+
    	<div class="row">
 			<div class="col-md-12">
 				<br>
