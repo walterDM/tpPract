c9d182f38151532316bb16e91af494b56502c384
walter <walter.m.d92@gmail.com>
2020-09-05 21:02:06 -0300

    agregando proveedores a productos

--- altaProducto.php
+++ altaProducto.php
@@ -2,6 +2,7 @@
 		 require("conexion.php");
 
 		 $select=mysqli_query($conexion,"SELECT idTipoProducto, descripcion FROM tiposproductos ORDER BY descripcion ASC");
+		 $selectProv=mysqli_query($conexion,"SELECT idProveedor, empresa FROM proveedores ORDER BY empresa ASC");
 		 $select3=mysqli_query($conexion,"SELECT * from puestofisico as pf where not exists (select idPuestoFisico from productos as p where p.idPuestoFisico = pf.idPuestoFisico)");
     ?>
      	<script language="javascript">
@@ -86,12 +87,25 @@
 								 </div>
 							 </div>
 							</div>
-							<div class="col-md-12">
-							<div class="form-group">
-			    <div class="contenido"><label for="imagen">imagen</label>
-			    <input type="file" name="imagen" class="form-control" id="imagen" ></div>
-			  </div>
-		            	  </div>
+							<div class="col-md-6">
+								<div class="form-group">
+								     <label>Proveedor</label>
+								       <select class="form-control" id="cbxProv" name="cbxProv">
+										<option value="0">seleccionar Proveedor</option>
+											<?php while ($rsProv = $selectProv->fetch_assoc()) {?>
+												<option value="<?php echo $rsProv['idProveedor']; ?>"><?php echo $rsProv['empresa']; ?>
+												</option>
+											<?php } ?>
+									   </select>
+								</div>
+		            	 	 </div>
+							<div class="col-md-6">
+								<div class="form-group">
+								    <div class="contenido"><label for="imagen">imagen</label>
+								   	 <input type="file" name="imagen" class="form-control" id="imagen" >
+									</div>
+							  	</div>
+		            	 	 </div>
 					  	  <div class="col-md-12" align="center">
 					  	   <div class="form-group">
 		          			   <button style="background:orange;color:white;width:30%" type="submit" name="guardar" value="guardar" id="btn2" class="btn btn-light">guardar producto</button>
--- modProducto.php
+++ modProducto.php
@@ -74,87 +74,119 @@
 		            		   <label>Nombre</label>
 		            		   <input type="text" class="form-control" name="descripcion" id="descripcion" value="<?php echo $datos['descripcion'];?>" placeholder="ingrese nombre del producto">
 		        	  	   </div>
-		        	  	   <div class="form-group">
-						       <label>LOTE</label>
-                               <input type="text" class="form-control" name="lote" id="lote" value="<?php echo $datos['Lote'];?>" placeholder="ingrese lote">
-                               <input type="hidden" name="id" id="id" value="<?php echo $id; ?>">
-                             </div>
-                             <div class="form-group">
-						       <label>Tipo de producto</label>
-						       <select id="cbxTipoProducto" name="cbxTipoProducto" class="form-control" >
-						       	<option value="0">seleccion</option>
-								 <?php $consulta3=mysqli_query($conexion,"SELECT idTipoProducto, descripcion FROM tiposproductos ORDER BY descripcion ASC");
-								   while($r=mysqli_fetch_array($consulta3)){?>
-								 	
-								  <option value="<?php echo $r['idTipoProducto'];?>" <?php  if($idTipoProducto==$r['idTipoProducto']){ echo'selected';}?>><?php echo $r['descripcion'];?></option>
-								<?php }?>
-							   </select>
-                             </div>
-                             <div class="form-group">
-							   <label>Marca</label>
-						         <select id="cbxMarca" name="cbxMarca"  class="form-control">
-						         	<option value="0">seleccion marca</option>
-								 <?php  while($r=mysqli_fetch_array($consulta5)){?>
-								  <option value="<?php echo $r['idMarca'] ?>"<?php if($idMarca==$r['idMarca']) echo 'Selected'?>><?php echo $r['nombreMarca'];?></option>
-								 <?php }?>
-							    </select>
-                             </div>
-					  	  </div>
-					  	  <div class="col-md-6">
+		        	  	  </div><!--endnombre-->
+		        	  	  <div class="col-md-6">
                             <div class="form-group">
 						       <label>Fecha de caducidad</label>
 						       <input type="date" class="form-control" name="fechaCaducidad" id="fechaCaducidad" value="<?php echo $datos['fechaCaducidad'];?>">
 							 </div>
-							 <div class="row">
-								<div class="col-md-6">
+						  </div>	 
+		        	  	</div><!--endRow Nombre y Fecha-->
+		        	  	<div class="row">
+		        	  		<div class="col-md-6">
+		        	  			<div class="form-group">
+							       <label>Tipo de producto</label>
+							       <select id="cbxTipoProducto" name="cbxTipoProducto" class="form-control" >
+							       	<option value="0">seleccion</option>
+									 <?php $consulta3=mysqli_query($conexion,"SELECT idTipoProducto, descripcion FROM tiposproductos ORDER BY descripcion ASC");
+									   while($r=mysqli_fetch_array($consulta3)){?>
+									 	
+									  <option value="<?php echo $r['idTipoProducto'];?>" <?php  if($idTipoProducto==$r['idTipoProducto']){ echo'selected';}?>><?php echo $r['descripcion'];?></option>
+									<?php }?>
+								   </select>
+	                             </div>
+		        	  		</div>
+		        	  		<div class="col-md-6">
+		        	  			 <div class="form-group">
+							       <label>LOTE</label>
+	                               <input type="text" class="form-control" name="lote" id="lote" value="<?php echo $datos['Lote'];?>" placeholder="ingrese lote">
+	                               <input type="hidden" name="id" id="id" value="<?php echo $id; ?>">
+	                             </div>
+		        	  		</div>
+		        	  	</div><!--end row tp y lote-->
+		        	  	<div class="row">
+		        	  		<div class="col-md-6">
+		        	  			<div class="form-group">
+								   <label>Marca</label>
+							         <select id="cbxMarca" name="cbxMarca"  class="form-control">
+							         	<option value="0">seleccion marca</option>
+									 <?php  while($r=mysqli_fetch_array($consulta5)){?>
+									  <option value="<?php echo $r['idMarca'] ?>"<?php if($idMarca==$r['idMarca']) echo 'Selected'?>><?php echo $r['nombreMarca'];?></option>
+									 <?php }?>
+								    </select>
+	                             </div>
+		        	  		</div>
+		        	  		<div class="col-md-3">
 					  	           <div class="form-group">
 						               <label>Cantidad</label>
 						               <input type="text" class="form-control" name="cantidadProd" id="cantidadProd" value="<?php echo $datos['cantidadProd'];?>" placeholder="ingrese cantidad">
 									 </div>
-								</div>
-								<div class="col-md-6">
+							</div>
+							<div class="col-md-3">
 					  	           <div class="form-group">
 						               <label>Precio</label>
 						               <input type="text" class="form-control" name="precio" id="precio" value="<?php echo $datos['precio'];?>" placeholder="ingrese precio">
 									 </div>
-								</div>
-							 </div>
-					  	   <div class="form-group">
-						       <label>Estado</label>
-						       <select class="form-control" id="estado" name="estado">
-								  <option <?php if($datos['estado']=="Activo") echo 'Selected'?>>Activo</option>
-								  <option <?php if($datos['estado']=="Inactivo") echo 'Selected'?>>Inactivo</option>
-							   </select>
-							 </div>
-							 <div class="row">
-								 <div class="col-md-12">
-								 <div class="form-group">
-						       <label>Estante</label>
-						       <select class="form-control" id="idPuestoFisico" name="idPuestoFisico">
-								 <?php  while($r=mysqli_fetch_array($consulta6)){?>
-								  <option <?php if($idPuestoFisico==$r['idPuestoFisico']) echo 'Selected'?> value="<?php echo $r['idPuestoFisico']?>"><?php echo $r['estante']."-".$r['fila']."-".$r['columna'];?></option>
-								<?php }?>
-							   </select>
-							 </div>
-								 </div>
-							 </div>
 							</div>
-							<div class="col-md-6">
+
+		        	  	</div><!--end row marca, cant y precio-->
+		        	  	<div class="row">
+		        	  		 <div class="col-md-6">
+		        	  		 	<div class="form-group">
+							       <label>Proveedor</label>
+							       <select id="cbxProv" name="cbxProv" class="form-control" >
+							       	<option value="0">seleccion</option>
+									 <?php $consulta3=mysqli_query($conexion,"SELECT idTipoProducto, descripcion FROM tiposproductos ORDER BY descripcion ASC");
+									   while($r=mysqli_fetch_array($consulta3)){?>
+									 	
+									  <option value="<?php echo $r['idTipoProducto'];?>" <?php  if($idTipoProducto==$r['idTipoProducto']){ echo'selected';}?>><?php echo $r['descripcion'];?></option>
+									<?php }?>
+								   </select>
+	                             </div>
+		        	  		 </div>
+								
+		            	 	 <div class="col-md-6">
+								 <div class="form-group">
+							       <label>Estante</label>
+							       <select class="form-control" id="idPuestoFisico" name="idPuestoFisico">
+									 <?php  while($r=mysqli_fetch_array($consulta6)){?>
+									  <option <?php if($idPuestoFisico==$r['idPuestoFisico']) echo 'Selected'?> value="<?php echo $r['idPuestoFisico']?>"><?php echo $r['estante']."-".$r['fila']."-".$r['columna'];?></option>
+									<?php }?>
+								   </select>
+								 </div>
+							</div>
+		        	  		
+		        	  	</div><!--end row prov y esntante-->
+		        	  	<div class="row">
+                           <div class="col-md-6">
+			        	  		 <div class="form-group">
+							       <label>Estado</label>
+							       <select class="form-control" id="estado" name="estado">
+									  <option <?php if($datos['estado']=="Activo") echo 'Selected'?>>Activo</option>
+									  <option <?php if($datos['estado']=="Inactivo") echo 'Selected'?>>Inactivo</option>
+								   </select>
+								 </div>
+		        	  		</div>  
+                        </div><!--end row Estado-->
+                        <div class="row">
+                        	<div class="col-md-6">
 							  <div class="form-group">
 			                    <div class="contenido"><label for="imagen">imagen</label>
 			                    <input type="file" name="imagen" class="form-control" id="imagen" ></div>
 			                  </div>
                             </div>
                             <div class="col-md-6">
-                            <img src="<?php echo "imagenes/". $datos["imagen"]; ?>" width =100>
+                           	 <img src="<?php echo "imagenes/". $datos["imagen"]; ?>" width =100>
                             </div>
-					  	  <div class="col-md-12" align="center">
+                        </div><!--end row Img-->
+						<div class="row justify-content-center">
+							<div class="col-md-6" align="center">
 					  	   <div class="form-group">
 		          			   <button style="background:orange;color:white;width:30%;margin-top:10%" type="submit" name="Modificar" value="Modificar" id="btn2" class="btn btn-light">Actualizar Producto</button>
 		            	   </div>
 		            	  </div>
-		            	</div>
-                       </form>
+						</div>	
+                      </form>
     <?php }?>
           </div>
       </div>
