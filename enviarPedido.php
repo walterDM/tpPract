3aef17c45783a87bc240e8f00da86de12e01590a
walter <walter.m.d92@gmail.com>
2020-09-09 13:13:28 -0300

    dando forma a enviar Pedido

---
+++ enviarPedido.php
@@ -0,0 +1,7 @@
+<?php 
+if (isset($_GET['seleccionado']) && !empty($_GET['seleccionado'])) {
+	foreach ($_GET['seleccionado'] as $s) {
+		Select
+	};
+}
+ ?>
--- listarProducto.php
+++ listarProducto.php
@@ -4,15 +4,22 @@ require 'conexion.php';
 $grupo=mysqli_query($conexion,"SELECT p.nombrePermiso FROM permisos AS p, grupospermisos AS up WHERE (p.nombrePermiso='buscar proveedor' OR p.nombrePermiso='baja proveedor' OR p.nombrePermiso='modificar proveedor') AND p.idPermiso=up.idPermiso AND up.idGrupo='$idGrupo'"); 
 $consultaProv="SELECT * from proveedores";
 $query=mysqli_query($conexion,$consultaProv);
-$queryBP="SELECT p.idProducto,p.descripcion,pr.empresa, tpp.idTpMarca,tpp.precio, m.nombreMarca,tp.descripcion as tipoProducto FROM productos p 
-JOIN productostpmarcas tpp on p.idProducto=tpp.idProducto
-JOIN proveedores pr on tpp.idProveedor=pr.idProveedor
-JOIN tiposproductos_marcas tpm on tpm.idTpMarca=tpp.idTpMarca
-JOIN marcas m on m.idMarca= tpm.idMarca
-JOIN tiposproductos tp on tp.idTipoProducto=tpm.idTipoProducto
-WHERE pr.empresa LIKE '%zink%'";
-$resultBP=mysqli_query($conexion,$queryBP);
-$totalProductos=mysqli_num_rows($resultBP);
+if (isset($_GET['cbxProv']) && !empty($_GET['cbxProv'])) {
+	$idProv=$_GET['cbxProv'];
+	$queryBP="SELECT p.idProducto,p.descripcion,pr.empresa, tpp.idTpMarca,tpp.precio, m.nombreMarca,tp.descripcion as tipoProducto FROM productos p 
+	JOIN productostpmarcas tpp on p.idProducto=tpp.idProducto
+	JOIN proveedores pr on tpp.idProveedor=pr.idProveedor
+	JOIN tiposproductos_marcas tpm on tpm.idTpMarca=tpp.idTpMarca
+	JOIN marcas m on m.idMarca= tpm.idMarca
+	JOIN tiposproductos tp on tp.idTipoProducto=tpm.idTipoProducto
+	WHERE pr.idProveedor=$idProv";
+	$resultBP=mysqli_query($conexion,$queryBP);
+	$totalProductos=mysqli_num_rows($resultBP);
+	$queryCT="SELECT descripcion from contactosproveedores where idProveedor=$idProv and idTipoContacto=2";
+	$resultCT=mysqli_query($conexion,$queryCT);
+	$queryCE="SELECT descripcion from contactosproveedores where idProveedor=$idProv and idTipoContacto=1";
+	$resultCE=mysqli_query($conexion,$queryCE);
+}
 ?>
 <?php if ($r=mysqli_fetch_array($grupo)) {?>
 	<br>
@@ -25,93 +32,75 @@ $totalProductos=mysqli_num_rows($resultBP);
 				<select class="form-control" id="cbxProv" name="cbxProv">
 					<option>seleccione proveedor</option>
 					<?php while ($r=mysqli_fetch_array($query)):?>
-						<option value="<?php echo $r['idProveedor']; ?>"> <?php echo $r['empresa']; ?></option>
-					<?php endwhile ?>
-				</select>
+						<option value="<?php echo $r['idProveedor'];?>" 
+							<?php if($r['idProveedor']==$idProv){echo 'Selected';}?>> <?php echo $r['empresa']; ?></option>
+						<?php endwhile ?>
+					</select>
+				</div>
+				<div class="col-md-2 bList">
+					<button type="submit"><i class="fas fa-search"></i></i></button>
+				</div>
 			</div>
-			<div class="col-md-2 bList">
-				<button type="submit"><i class="fas fa-search"></i></i></button>
-			</div>
-		</div>
-	</form>
-	
-	<br>
-<div class="col-md-12">
-	<div id="result" style="border: 1px solid white;overflow-y: scroll;background:#fafafa;padding-top:15px">
-		<table class="table striped" style="background:#fafafa;height:300px">
-			<thead>
-				<th style="text-align: center;">Producto</th>
-				<th style="text-align: center;">Proveedor</th>
-				<th style="text-align: center;">Tipo Producto</th>
-				<th style="text-align: center;">Marca</th>
-				<!--<th>Proveedor</th>-->
-				<th style="text-align: center;">ultimo Precio</th>
-				<th style="text-align: center;">Email Proveedor</th>
-				<th style="text-align: center;">Tel Proveedor</th>
-				<th></th>
-				<th></th>
-				<th></th>
-			</thead>
-			<tbody>
-				<?php while ($row=mysqli_fetch_array($resultBP)): ?>
-					<tr>
+		</form>
 
-						<td style="text-align: center"><?php echo $row['descripcion'];?></td>
-						<td style="text-align: center"><?php echo $row['empresa'];?></td>
-						<td style="text-align: center"><?php echo $row['tipoProducto'];?></td>
-						<td style="text-align: center"><?php echo $row['nombreMarca']; //while($r=mysqli_fetch_array($select2)){ echo $r['descripcion'];}?></td>
-						<td style="text-align: center"><?php echo $row['precio'];?></td>
-						
-						<td style="text-align: center"><?php// echo $Telefono;?></td>
-						<td></td>
-						<td></td>
+		<br>
+		
+		<form action="enviarPedido.php" method="GET">
+			<div class="row justify-content-center">
+				<div class="col-md-12">.
+					<div class="form-group">	
+						<div id="result" style="border: 1px solid white;overflow-y: scroll;background:#fafafa;padding-top:15px">
+							<table class="table striped" style="background:#fafafa;height:300px">
+								<thead>
+									<th style="text-align: center;">Producto</th>
+									<th style="text-align: center;">Proveedor</th>
+									<th style="text-align: center;">Tipo Producto</th>
+									<th style="text-align: center;">Marca</th>
+									<!--<th>Proveedor</th>-->
+									<th style="text-align: center;">ultimo Precio</th>
+									<th style="text-align: center;">Email Proveedor</th>
+									<th style="text-align: center;">Tel Proveedor</th>
+									<th></th>
+									<th></th>
+									<th></th>
+								</thead>
+								<tbody>
+									<?php while ($row=mysqli_fetch_array($resultBP)): ?>
+										<tr>
 
-					
-							<td><a href="#" style="border-radius:30px;font-size:20px" class="btn btn-light" data-toggle="modal" data-target="#info<?php echo $idPersona; ?>"><i class="fas fa-trash-alt"></i></a></td>
-						
-								<td>
-									<form method="POST" action="modificarUsuario.php">
-										<button style="border-radius:30px;font-size:20px" type="submit" name="idPersona" value="<?php echo $row['idPersona'];?>" class="btn btn-light"><i class="fas fa-pencil-alt"></i></button>
-									</form>
-								</td>
-							
-					</tr>
-				<?php endwhile ?>
-				<div data-backdrop="static"  class="modal fade" id="info<?php //echo $idPersona;?>">
-					<div class="col-md-12 modal-dialog" >
-						<div class="modal-content">
-							<div class="modal-header">
-								<h4 class="modal-title">Baja Usuario</h4>
-								<button type="button" class="close" data-dismiss="modal">X</button>
-							</div>
-							<div class="col-md-12" style="background:#e0e0e0">
-								<div class="modal-body" >
-									<h5 align="center">Estas seguro que deseas eliminar el siguiente usuario:</h5>
-									<h6 align="center"><?php echo $row['usuario'];?></h6>
-									<div align="center">
-										<form method="POST" action="ABMusuario.php">
-											<input type="text" class="form-control" name="idPersona" id="idPersona" value="<?php echo $row['idPersona'];?>" hidden>
-											<button  type="submit" name="eliminarUsuario" value="eliminarUsuario" class="btn btn-light">Eliminar</button>
-										</form>
-										<button type="button" class="btn btn-light" data-dismiss="modal">Cancelar</button>
-									</div>
-								</div>
-							</div>	
+											<td style="text-align: center"><?php echo $row['descripcion'];?></td>
+											<td style="text-align: center"><?php echo $row['empresa'];?></td>
+											<td style="text-align: center"><?php echo $row['tipoProducto'];?></td>
+											<td style="text-align: center"><?php echo $row['nombreMarca'];?></td>
+											<td style="text-align: center"><?php echo $row['precio'];?></td>
+											<td style="text-align: center"><?php while($r=mysqli_fetch_array($resultCE)){ echo $r['descripcion'];}?></td>
+											<td style="text-align: center"><?php while($rs=mysqli_fetch_array($resultCT)){ echo $rs['descripcion'];}?></td>
+											<td><input type="checkbox" name="seleccionado[]" value="<?php echo $row['idTpMarca']?>"></td>
+											<td></td>
+										</tr>
+									<?php endwhile ?>
+									
+								</tbody>
+								<tfoot>
+									<tr align="center">
+										<th colspan="12">Cantidad de registros encontrados: <?php echo $totalProductos?></th>
+									</tr>
+								</tfoot>
+							</table>
 						</div>
 					</div>
 				</div>
-				<?php             
-				//} 
-				?>  
-			</tbody>
-			<tfoot>
-				<tr align="center">
-					<th colspan="12">Cantidad de registros encontrados: <?php echo $totalProductos?></th>
-				</tr>
-			</tfoot>
-		</table>
-	</div>
-</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-6" align="center">
+					<div class="form-group">
+						<button style="background:orange;color:white;width:30%;" type="submit"  id="btn2" class="btn btn-light">Enviar Pedido</button>
+					</div>
+				</div>
+			</div>
+
+		</form>
+		
 
 
 
@@ -122,11 +111,11 @@ $totalProductos=mysqli_num_rows($resultBP);
 
 
 
-<?php }else{?>
-	<div class="col-md-12" style="padding-top:10px">
-		<div class="alert alert-warning" role="alert">
-			<h2 align="center">ACCESO DENEGADO</h2>
+	<?php }else{?>
+		<div class="col-md-12" style="padding-top:10px">
+			<div class="alert alert-warning" role="alert">
+				<h2 align="center">ACCESO DENEGADO</h2>
+			</div>
 		</div>
-	</div>
-<?php } ?>
-<?php require 'footer.php'; ?>
+	<?php } ?>
+	<?php require 'footer.php'; ?>
