d3b7bf6bc27dee703e379457a813788037794500
walter <walter.m.d92@gmail.com>
2020-09-09 13:55:47 -0300

    enviarPedidoProveedor

--- enviarPedido.php
+++ enviarPedido.php
@@ -1,7 +1,16 @@
 <?php 
+require 'conexion.php';
 if (isset($_GET['seleccionado']) && !empty($_GET['seleccionado'])) {
 	foreach ($_GET['seleccionado'] as $s) {
-		Select
+		$queryMTP="SELECT m.nombreMarca, tp.descripcion FROM marcas as m
+					JOIN tiposproductos_marcas as tpm  on tpm.idMarca= m.idMarca 
+					JOIN tiposproductos as tp on tpm.idTipoProducto=tp.idTipoProducto
+					where tpm.idTpMarca=$s";
+		$resultMTP=mysqli_query($conexion,$queryMTP);
+		while ($r=mysqli_fetch_array($resultMTP)) {
+			echo "TIPO PRODUCTO: ".$r['descripcion']."<br>";
+			echo "Marca: ".$r['nombreMarca']."<br>";
+		}
 	};
 }
- ?>
+?>
