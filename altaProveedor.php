ea9e7b9ff89888f9379d25bb1d8903aa722c78da
MercedesGraterox <mercedesgraterox@gmail.com>
2020-09-23 12:52:54 -0300

    modificado por tefy

--- altaProveedor.php
+++ altaProveedor.php
@@ -11,7 +11,36 @@ while ($r=mysqli_fetch_array($select)) {
 }
 // $select2=mysqli_query($conexion,"SELECT * FROM grupos");
 
+$select2=mysqli_query($conexion,"SELECT * FROM grupos WHERE nombreGrupo!='CLIENTE'");
+$select3=mysqli_query($conexion,"SELECT idPais,nombrePais FROM paises ORDER BY nombrePais ASC");
+$select4=mysqli_query($conexion,"SELECT idProvincia,nombreProvincia FROM provincias ORDER BY nombreProvincia ASC");
+$select5=mysqli_query($conexion,"SELECT idTipoDomicilio,descripcion FROM tiposdomicilios ORDER BY descripcion ASC");
+
 ?>
+
+<script language="javascript">
+  $(document).ready(function(){
+    $("#cbxpais").change(function () {  
+      $("#cbxpais option:selected").each(function () {
+        id_estado = $(this).val();
+        $.post("includes/getProvincia.php", { id_estado: id_estado }, function(data){
+          $("#cbxprovincia").html(data);
+        });            
+      });
+    });
+     $("#cbxprovincia").change(function () {  
+      $("#cbxprovincia option:selected").each(function () {
+        id_ciudad = $(this).val();
+        $.post("includes/getCiudad.php", { id_ciudad: id_ciudad }, function(data){
+          $("#cbxciudad").html(data);
+        });            
+      });
+    });
+  });
+  
+  
+  
+ </script>
 
 <div class="row">
   
@@ -29,20 +58,73 @@ while ($r=mysqli_fetch_array($select)) {
                <input type="text" class="form-control" name="empresa" id="empresa"  placeholder="Ingrese el nombre de la empresa">
              </div>
            </div>
-
-           <div class="col-md-6">
-             <div class="form-group">
-               <label>Dirección</label>
-               <input type="text" class="form-control" name="direccion" id="direccion" placeholder="Ingrese la dirección">
-             </div>
-           </div>
-
-           <div class="col-md-6">
+            <div class="col-md-6">
             <div class="form-group">
              <label>Cuit</label>
              <input type="text" class="form-control" name="cuit" id="cuit" placeholder="Ingrese el cuit">
            </div>
          </div>
+             <div class="col-md-6">
+              <div class="form-group">
+               <label>Pais</label>
+               <select class="form-control" id="cbxpais" name="cbxpais">
+                <option>Seleecione Pais</option>
+                 <?php while ($rsTP = $select3->fetch_assoc()){?>
+                 <option><?php echo $rsTP['nombrePais'];?></option>
+                 <?php } ?>
+               </select>
+             </div>
+           </div>
+             <div class="col-md-6">
+             <div class="form-group">
+               <label>Ciudad</label>
+               <select class="form-control" id="cbxciudad" name="cbxciudad">
+                 <option>Seleccione ciudad</option>
+               </select>
+             </div>
+           </div>
+
+           <div class="col-md-6">
+           <div class="form-group">
+               <label>Provincia</label>
+               <select class="form-control" id="cbxprovincia" name="cbxprovincia">
+                   <option>seleccione provincia</option>
+               </select>
+           </div>
+         </div>
+        
+          <div class="col-md-6">
+           <div class="form-group">
+               <label>tipo de domicilio</label>
+               <select class="form-control" id="tipodomicilio" name="tipodomicilio">
+               <?php while ($rsTP = $select5->fetch_assoc()){?>
+                 <option value="<?php echo $rsTP['idTipoDomicilio']; ?>"><?php echo $rsTP['descripcion'];?></option>
+               <?php } ?>
+               </select>
+           </div>
+         </div>
+          <!--   <div class="row"> -->
+                <div class="form-group col-md-6">
+                   <label>Calle</label>
+                   <input type="text" class="form-control" name="calle" id="calle"  placeholder="ingrese calle">
+                </div>
+                <div class="form-group col-md-6">
+                   <label>Altura</label>
+                   <input type="text" class="form-control" name="altura" id="altura"  placeholder="ingrese altura">
+                </div>
+             <!-- </div> -->
+          <!--   <div class="row"> -->
+                <div class="form-group col-md-6">
+                   <label>Depto</label>
+                   <input type="text" class="form-control" name="dpto" id="dpto"  placeholder="ingrese departamento">
+                </div>
+
+                <div class="form-group col-md-6">
+                   <label>Piso</label>
+                   <input type="text" class="form-control" name="piso" id="piso"  placeholder="ingrese piso">
+                </div>
+            <!--  </div> -->
+
          
          <div class="col-md-6">
           <div class="form-group">
@@ -57,7 +139,7 @@ while ($r=mysqli_fetch_array($select)) {
        </div>
      </div>
      
-     <div class="col-md-6">
+     <div class="col-md-12">
        <div class="form-group">
          <label>Descripción</label>
          <input type="text" class="form-control" name="descripcion" id="descripcion" placeholder="Descripción">
